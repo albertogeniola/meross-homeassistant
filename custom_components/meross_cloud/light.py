bf153096c33fd2363c50f51756bef26806b064c7
Tom Whittock <tom.whittock@gmail.com>
2019-10-28 09:59:22 +0000

    Do not issue multiple changes in a single message, add debugging.
    * When sending a message with multiple capabilites, the lightbulb itself
    will respond correctly, but the cloud server responds with a second
    message undoing any colour change.

--- custom_components/meross_cloud/light.py
+++ custom_components/meross_cloud/light.py
@@ -8,6 +8,18 @@ from .common import (calculate_switch_id, DOMAIN, ENROLLED_DEVICES, MANAGER)
 
 _LOGGER = logging.getLogger(__name__)
 
+def rgb_int_to_tuple(color):
+    blue = color & 255
+    green = (color >> 8) & 255
+    red = (color >> 16) & 255
+    return (red, green, blue)
+
+def expand_status(status):
+    "Expand the status information for readability"
+    out = dict(status)
+    out['rgb'] = rgb_int_to_tuple(status['rgb'])
+    return out
+
 class LightEntityWrapper(Light):
     """Wrapper class to adapt the Meross switches into the Homeassistant platform"""
     _device = None
@@ -28,6 +40,7 @@ class LightEntityWrapper(Light):
         self._device.register_event_callback(self.handler)
 
     def handler(self, evt):
+        _LOGGER.debug("event_handler(name=%r, evt=%r)" % (self._device_name, repr(vars((evt)))))
         self.async_schedule_update_ha_state(False)
 
     @property
@@ -52,59 +65,55 @@ class LightEntityWrapper(Light):
         return self._device.get_channel_status(self._channel_id).get('onoff')
 
     def turn_off(self, **kwargs) -> None:
-        _LOGGER.debug('turn_off(chan=%r)' % self._channel_id)
+        _LOGGER.debug('turn_off(name=%r)' % self._device_name)
         self._device.turn_off(channel=self._channel_id)
 
     def turn_on(self, **kwargs) -> None:
-        _LOGGER.debug('turn_on(chan=%r, %r)' % (self._channel_id, kwargs))
-        self._device.turn_on(channel=self._channel_id)
+        if not self.is_on:
+            _LOGGER.debug('turn_on(name=%r)' % (self._device_name))
+            self._device.turn_on(channel=self._channel_id) # Avoid a potential response event race between setting on and below set_light_color
 
         # Color is taken from either of these 2 values, but not both.
-        rgb = None
-        temperature = -1
         if ATTR_HS_COLOR in kwargs:
             h, s = kwargs[ATTR_HS_COLOR]
             rgb = color_util.color_hsv_to_RGB(h, s, 100)
-            _LOGGER.debug("    color conversion: rgb=%r -- h=%r s=%r" % (rgb, h, s))
+            _LOGGER.debug("color change: rgb=%r -- h=%r s=%r" % (rgb, h, s))
+            self._device.set_light_color(self._channel_id, rgb=rgb)
         elif ATTR_COLOR_TEMP in kwargs:
             mired = kwargs[ATTR_COLOR_TEMP]
             norm_value = (mired - self.min_mireds) / (self.max_mireds - self.min_mireds)
             temperature = 100 - (norm_value * 100)
-            _LOGGER.debug("    temperature conversion: mired=%r meross=%r" % (mired, temperature))
+            _LOGGER.debug("temperature change: mired=%r meross=%r" % (mired, temperature))
+            self._device.set_light_color(self._channel_id, temperature=temperature)
 
         # Brightness must always be set, so take previous luminance if not explicitly set now.
         brightness = self._device.get_light_color(self._channel_id).get('luminance')
         if ATTR_BRIGHTNESS in kwargs:
             brightness = kwargs[ATTR_BRIGHTNESS] * 100 / 255
-            _LOGGER.debug("brightness change: %r" % brightness)
-
-        _LOGGER.debug('    set_light_color(chan=%r, rgb=%r, luminance=%r, temperature=%r)' % (self._channel_id, rgb, brightness, temperature))
-        self._device.set_light_color(self._channel_id, rgb=rgb, luminance=brightness, temperature=temperature)
+            _LOGGER.debug("    brightness change: %r" % brightness)
+            self._device.set_light_color(self._channel_id, luminance=brightness)
 
     @property
     def brightness(self):
         # Meross bulbs support luminance between 0 and 100;
         # while the HA wants values from 0 to 255. Therefore, we need to scale the values.
         status = self._device.get_status(self._channel_id)
-        _LOGGER.debug('get_brightness status=%r' % status)
+        _LOGGER.debug('get_brightness(name=%r status=%r)' % (self._device_name, expand_status(status)))
         return status.get('luminance') / 100 * 255
 
     @property
     def hs_color(self):
         status = self._device.get_channel_status(self._channel_id)
-        _LOGGER.debug('get_hs_color status=%r' % status)
+        _LOGGER.debug('get_hs_color(name=%r status=%r)' % (self._device_name, expand_status(status)))
         if status.get('capacity') == 5: # rgb mode
-            color = status.get('rgb')
-            blue = color & 255
-            green = (color >> 8) & 255
-            red = (color >> 16) & 255
-            return color_util.color_RGB_to_hs(red, green, blue)
+            rgb = rgb_int_to_tuple(status.get('rgb'))
+            return color_util.color_RGB_to_hs(*rgb)
         return None
 
     @property
     def color_temp(self):
         status = self._device.get_channel_status(self._channel_id)
-        _LOGGER.debug('get_color_temp status=%r' % status)
+        _LOGGER.debug('get_color_temp(name=%r status=%r)' % (self._device_name, expand_status(status)))
         if status.get('capacity') == 6: # White light mode
             value = status.get('temperature')
             norm_value = (100 - value) / 100.0
@@ -113,7 +122,14 @@ class LightEntityWrapper(Light):
 
     @property
     def supported_features(self):
-        return SUPPORT_BRIGHTNESS | SUPPORT_COLOR | SUPPORT_COLOR_TEMP
+        flags = 0
+        if self._device.supports_luminance():
+           flags |= SUPPORT_BRIGHTNESS
+        if self._device.is_rgb():
+           flags |= SUPPORT_COLOR
+        if self._device.is_light_temperature():
+           flags |= SUPPORT_COLOR_TEMP
+        return flags
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
