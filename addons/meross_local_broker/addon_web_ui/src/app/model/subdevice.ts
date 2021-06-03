d0832d302254fa2323a6b139a1bfe23490daf59f
Alberto Geniola <albertogeniola@gmail.com>
2021-06-03 22:22:56 +0200

    Implemented SubDevice listing

---
+++ addons/meross_local_broker/addon_web_ui/src/app/model/subdevice.ts
@@ -0,0 +1,9 @@
+export interface Subdevice {
+  readonly sub_device_id: string;
+  readonly true_id: string;
+  readonly sub_device_type: string;
+  readonly sub_device_vendor: string;
+  readonly sub_device_name: string;
+  readonly sub_device_icon_id: string;
+  readonly hub_uuid: string;
+}
