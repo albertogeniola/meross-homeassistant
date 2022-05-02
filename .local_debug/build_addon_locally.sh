7a38b48e8c3115e226eeee4b10979cc467d2afdd
Alberto Geniola <albertogeniola@gmail.com>
2022-05-02 09:23:00 +0200

    Testing DockerHub pipeline build

--- .local_debug/build_addon_locally.sh
+++ .local_debug/build_addon_locally.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Note: on windows + WSL, in case of port binding issues,
+# you might need to run "net stop nat" and "net start nat" within
+# a prompt with admin rights.
 docker build --build-arg BUILD_ARCH="amd64" --build-arg BUILD_FROM="ghcr.io/home-assistant/amd64-base-debian:buster" -t local/meross_local_broker "addons/meross_local_broker"
